24019cde40b22d6427bd582062d937cdfbb833ab
huy75 <huy.nguyen66@gmail.com>
2020-10-25 14:44:49 +0000

    fix add extra tests

--- main_Test/main.c
+++ main_Test/main.c
@@ -1,5 +1,3 @@
-#include <limits.h>
-#include <stdio.h>
 #include "../holberton.h"
 
 /**
@@ -9,27 +7,62 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
-/*	unsigned int ui;*/
-/*	void *addr;*/
+	int alen, elen;
 
-	len = _printf("*Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	int p = 237482;
+        int n = -328472;
+        int z = 0;
 
-	_printf("*Length:[%d, %i] xxxx\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
+/*	printf(NULL); */
+	_printf(NULL);
+	printf("START OF TEST\n");
 
-        _printf("*Character:[%c]\n", 'H');
-        printf("Character:[%c]\n", 'H');
+	printf("=====================\n");
+	printf("*****INTEGER*****\n");
+	printf("=====================\n");
+	alen = elen = 0;
+	elen = printf("Expected   : %i\n", p);
+	alen = _printf("Actual     : %i\n", p);
+	printf("Expected   : %i\n", elen);
+	_printf("Actual     : %i\n", alen);
+	elen = printf("Expected   : %i\n", n);
+	alen = _printf("Actual     : %i\n", n);
+	printf("Expected   : %i\n", elen);
+	_printf("Actual     : %i\n", alen);
+	elen = printf("Expected   : %i\n", z);
+	alen = _printf("Actual     : %i\n", z);
+	printf("Expected   : %i\n", elen);
+	_printf("Actual     : %i\n", alen);
+	printf("Expected   : %i\n", INT_MAX);
+	_printf("Actual     : %i\n", INT_MAX);
+	printf("Expected   : %i\n", INT_MIN);
+	_printf("Actual     : %i\n", INT_MIN);
 
-	_printf("test: 3 * 6 = %d\n", 3 * 6);
 
-	_printf("*String:[%s]\n", "I am a string !");
-        printf("String:[%s]\n", "I am a string !");
+	printf("=====================\n");
+	printf("*****DECIMAL*****\n");
+	printf("=====================\n");
+	alen = elen = 0;
+	p = 1134001;
+	n = -4567;
+	z = 0;
+	elen = printf("Expected   : %d\n", p);
+	alen = _printf("Actual     : %d\n", p);
+	printf("Expected   : %d\n", elen);
+	_printf("Actual     : %d\n", alen);
+	elen = printf("Expected   : %d\n", n);
+	alen = _printf("Actual     : %d\n", n);
+	printf("Expected   : %d\n", elen);
+	_printf("Actual     : %d\n", alen);
+	elen = printf("Expected   : %i\n", z);
+	alen = _printf("Actual     : %i\n", z);
+	printf("Expected   : %i\n", elen);
+	_printf("Actual     : %i\n", alen);
+	printf("Expected   : %d\n", INT_MAX);
+	_printf("Actual     : %d\n", INT_MAX);
+	printf("Expected   : %d\n", INT_MIN);
+	_printf("Actual     : %d\n", INT_MIN);
 
-	_printf("*Modulo: %%\n");
-	printf("Modulo: %%\n");
 
 /*
 
