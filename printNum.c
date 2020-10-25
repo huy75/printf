bf04a5cc083b12bffebe4717eba0eee32808f6af
huy75 <huy.nguyen66@gmail.com>
2020-10-25 19:57:20 +0000

    fix add %b

--- printNum.c
+++ printNum.c
@@ -1,6 +1,44 @@
 #include "holberton.h"
 
 /**
+ * convert - converts number and base into string
+ * @num: input number
+ * @base: input base
+ * Return: result string
+ */
+char *convert(unsigned long int num, int base)
+{
+	static char *array;
+	static char buffer[50];
+	char *ptr;
+
+	array = "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = array[num % base];
+		num /= base;
+	} while (num);
+
+	return (ptr);
+}
+
+/**
+ * printBinary - prints binary number
+ * @ap: the argument
+ * Return: the number of bytes printed
+ */
+int printBinary(va_list ap)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+
+	char *str = convert(n, 2);
+
+	return (_puts(str));
+}
+
+
+/**
  * printInt - prints integer
  * @ap: the argument
  * Return: the number of chars printed.
